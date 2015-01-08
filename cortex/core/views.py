6a5faca926af7e772f321c1521583ae70cb228e1
James Durand <james.durand@alumni.msoe.edu>
2015-01-08 08:56:49 -0600

    Add simple root view

--- cortex/core/views.py
+++ cortex/core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+import django.views.generic
 
-# Create your views here.
+# pylint: disable=too-few-public-methods
+
+class RootView(django.views.generic.TemplateView):
+	template_name = 'root.html'
+
--- cortex/urls.py
+++ cortex/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+import cortex.core.views
+
 urlpatterns = patterns('',
-	# Examples:
-	# url(r'^$', 'cortex.views.home', name='home'),
-	# url(r'^blog/', include('blog.urls')),
+	url(r'^$', cortex.core.views.RootView.as_view(), name = 'root'),
 
 	url(r'^admin/', include(admin.site.urls)),
 )
+
