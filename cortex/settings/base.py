37f978d729aa33d90e2542259e98b825941f6a8f
James Durand <james.durand@alumni.msoe.edu>
2015-01-11 11:58:00 -0600

    Add login and logout capabilities

--- cortex/settings/base.py
+++ cortex/settings/base.py
@@ -16,6 +16,8 @@ BASE_DIR = os.path.dirname(
 	)
 )
 
+import django.core.urlresolvers
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'bootstrap3',
 	'cortex.core',
 ]
 
@@ -86,4 +89,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Authentication
+# https://docs.djangoproject.com/en/1.7/ref/settings/#auth
+
+AUTHENTICATION_BACKENDS = [
+	'django.contrib.auth.backends.ModelBackend',
+]
+
+LOGIN_URL = django.core.urlresolvers.reverse_lazy('login')
+LOGIN_REDIRECT_URL = django.core.urlresolvers.reverse_lazy('root')
+LOGOUT_URL = django.core.urlresolvers.reverse_lazy('logout')
 
--- cortex/urls.py
+++ cortex/urls.py
@@ -3,9 +3,27 @@ from django.contrib import admin
 
 import cortex.core.views
 
-urlpatterns = patterns('',
-	url(r'^$', cortex.core.views.RootView.as_view(), name = 'root'),
+# pylint: disable=invalid-name
 
-	url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('',
+	url(
+		r'^$',
+		cortex.core.views.RootView.as_view(),
+		name = 'root',
+	),
+	url(
+		r'^login$',
+		cortex.core.views.LoginView.as_view(),
+		name = 'login',
+	),
+	url(
+		r'^logout',
+		cortex.core.views.LogoutView.as_view(),
+		name = 'logout',
+	),
+	url(
+		r'^admin/',
+		include(admin.site.urls),
+	),
 )
 
