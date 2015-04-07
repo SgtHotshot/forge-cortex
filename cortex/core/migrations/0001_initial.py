3eebe6b9957a2ec056da48a83bd2ea2400d2aefd
Sgthotshot <sgt.hotshot@gmail.com>
2015-04-07 15:37:04 -0500

    Updated migration

--- cortex/core/migrations/0001_initial.py
+++ cortex/core/migrations/0001_initial.py
@@ -12,84 +12,67 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='mod',
+            name='Mod',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=40)),
-                ('modType', models.CharField(blank=True, max_length=1, choices=[('0', 'Forge'), ('1', 'Bukkit')])),
-                ('visibility', models.CharField(max_length=1, choices=[('0', 'Visible'), ('1', 'Hidden'), ('2', 'Private')])),
-                ('logo', models.ImageField(blank=True, upload_to=cortex.core.models.mod.generate_imagefilename)),
+                ('modType', models.CharField(choices=[('0', 'Forge'), ('1', 'Bukkit')], max_length=1, blank=True)),
+                ('visibility', models.CharField(choices=[('0', 'Visible'), ('1', 'Hidden'), ('2', 'Private')], max_length=1)),
+                ('logo', models.ImageField(upload_to=cortex.core.models.Mod.generate_imagefilename, blank=True)),
                 ('website', models.URLField()),
                 ('description', models.TextField()),
                 ('license', models.TextField()),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='modVersion',
+            name='ModVersion',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
-                ('version', models.CharField(blank=True, max_length=40)),
-                ('filename', models.CharField(blank=True, max_length=120)),
-                ('file', models.FileField(upload_to=cortex.core.models.modVersion.generate_filename)),
-                ('mod', models.ForeignKey(to='core.mod')),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('version', models.CharField(max_length=40, blank=True)),
+                ('filename', models.CharField(max_length=120, blank=True)),
+                ('file', models.FileField(upload_to=cortex.core.models.ModVersion.generate_filename)),
+                ('mod', models.ForeignKey(to='core.Mod')),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='person',
+            name='Person',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
                 ('firstName', models.CharField(max_length=20)),
                 ('lastName', models.CharField(max_length=20)),
                 ('isVerified', models.BooleanField(default=False)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='String',
+            name='Tag',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
                 ('value', models.CharField(max_length=255)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='modversion',
             name='tags',
-            field=models.ManyToManyField(to='core.String'),
-            preserve_default=True,
+            field=models.ManyToManyField(to='core.Tag'),
         ),
         migrations.AddField(
             model_name='mod',
             name='authors',
-            field=models.ForeignKey(to='core.person', blank=True),
-            preserve_default=True,
+            field=models.ForeignKey(to='core.Person', blank=True),
         ),
         migrations.AddField(
             model_name='mod',
             name='latest',
-            field=models.OneToOneField(to='core.modVersion', related_name='latest_mod'),
-            preserve_default=True,
+            field=models.OneToOneField(to='core.ModVersion', related_name='latest_mod'),
         ),
         migrations.AddField(
             model_name='mod',
             name='recommended',
-            field=models.OneToOneField(to='core.modVersion', related_name='recommended_mod'),
-            preserve_default=True,
+            field=models.OneToOneField(to='core.ModVersion', related_name='recommended_mod'),
         ),
         migrations.AddField(
             model_name='mod',
             name='tags',
-            field=models.ManyToManyField(blank=True, to='core.String'),
-            preserve_default=True,
+            field=models.ManyToManyField(to='core.Tag', blank=True),
         ),
     ]
