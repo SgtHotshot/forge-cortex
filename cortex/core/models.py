21a32122ec593ba04735fef0457fe235865fee1c
Sgthotshot <sgt.hotshot@gmail.com>
2015-04-07 15:25:44 -0500

    Refactored some of the models

--- cortex/core/models.py
+++ cortex/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 
@@ -14,42 +15,42 @@ VISIBILITY_CHOICES = (
 )
 
 
-	
+MODS = "mods"
+IMAGES = "images"
+FILES = "files"
 
 
 
-class String(models.Model):
+class Tag(models.Model):
 	value = models.CharField(max_length=255)
 
-class person(models.Model):
+class Person(models.Model):
 	firstName = models.CharField(max_length=20)
 	lastName = models.CharField(max_length=20)
 	isVerified = models.BooleanField(default=False)
-	
-class mod(models.Model):
+
+class Mod(models.Model):
 	def generate_imagefilename(self, filename):
-		url = "mods/%s/images/" % (self.mod.name)
-		return url
+		return os.path.join(MODS, self.Mod.name, IMAGES, filename)
 
 	name = models.CharField(max_length=40)
 	modType = models.CharField(max_length=1, choices=MOD_TYPE_CHOICES, blank=True)
 	visibility = models.CharField(max_length=1, choices=VISIBILITY_CHOICES)
 	logo = models.ImageField(upload_to=generate_imagefilename, blank=True)
-	authors = models.ForeignKey(person, blank=True)
-	recommended = models.OneToOneField('modVersion', related_name="recommended_mod")
-	latest = models.OneToOneField('modVersion', related_name="latest_mod")
+	authors = models.ForeignKey(Person, blank=True)
+	recommended = models.OneToOneField('ModVersion', related_name="recommended_mod")
+	latest = models.OneToOneField('ModVersion', related_name="latest_mod")
 	website = models.URLField()
 	description = models.TextField()
 	license = models.TextField()
-	tags = models.ManyToManyField(String, blank=True)
-	
-class modVersion(models.Model):
-	def generate_filename(self, filename):
-		url = "mods/%s/files/" % (self.mod.name)
-		return url
+	tags = models.ManyToManyField(Tag, blank=True)
 
-	tags = models.ManyToManyField(String)
+class ModVersion(models.Model):
+	def generate_filename(self, filename):
+		return os.path.join(MODS, self.Mod.name, FILES, filename)
+
+	tags = models.ManyToManyField(Tag)
 	version = models.CharField(max_length=40, blank=True)
 	filename = models.CharField(max_length=120, blank=True)
-	mod = models.ForeignKey(mod)
+	mod = models.ForeignKey(Mod)
 	file = models.FileField(upload_to=generate_filename)
